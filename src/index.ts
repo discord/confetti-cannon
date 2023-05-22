40750cc35595676d415961369a00d799409d8127
Michael Peterson <peterson@discordapp.com>
2023-05-21 19:30:19 -0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export { default as ConfettiCanvas } from "./components/ConfettiCanvas";
-export { default as SpriteCanvas } from "./components/SpriteCanvas";
+export { default as SpriteCanvas, SpriteProp } from "./components/SpriteCanvas";
+export { default as useConfettiCannon } from "./components/useConfettiCannon";
+export { default as Environment } from "./Environment";
+export {
+  default as createConfetti,
+  CreateConfettiArgs,
+} from "./createConfetti";
+export * from "./easing";
+export * from "./Types";
